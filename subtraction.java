a5ed3fa3afadeedb9859ff2e8ea94e43ac983384
김관진 <cozyschooll@gmail.com>
2023-03-10 18:55:31 +0900

    빼기 기능 소스 파일 커밋

---
+++ subtraction.java
@@ -0,0 +1,17 @@
+public class subtraction {
+
+    int a;
+    int b;
+    int resutl;
+
+    public subtraction(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getSubtraction() {
+        resutl = a - b;
+        return resutl;
+    }
+    
+}
